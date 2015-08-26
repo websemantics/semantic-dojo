5207582eff8ecf1664cd397fce8cbc57584f61b1
Adnan <adnan@websemantics.ca>
2015-08-26 17:04:09 +0200

    add config.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,31 @@
 *******************************/
 
 var
-  gulp = require('gulp-help')(require('gulp')),
-  less = require('gulp-less'),
-  minifyCSS    = require('gulp-minify-css');
+  gulp            = require('gulp-help')(require('gulp')),
+  less            = require('gulp-less'),
+  requireDotFile  = require('require-dot-file'),
+  minifyCSS       = require('gulp-minify-css'),
+  config;
+
+
+  /*******************************
+  Config
+  *******************************/
+
+  try {
+    config = requireDotFile('config.json');
+  }
+  catch(error) {
+    if(error.code === 'MODULE_NOT_FOUND') {
+      console.error('No config.json file found');
+    }
+  }
 
   gulp.task('default', function() {
    
+      console.info('Building Semantic-Dojo');
+      console.info(config);
+
       gulp.src(['less/semantic-dojo.less'])
         .pipe(less())
         .pipe(minifyCSS())
