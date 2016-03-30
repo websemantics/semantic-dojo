9711c459c8a29366318b83a865ec8a507c2a2053
Adnan <adnan@websemantics.ca>
2016-03-30 15:49:20 -0400

    Deploy to gh-pages script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This bash file will deploy the dist folder to the project gh-page
+set -e
+
+# Base directory for this entire project
+BASEDIR=$(cd $(dirname $0) && pwd)
+
+# Destination directory for built code
+DISTDIR="$BASEDIR/dist"
+
+# run the build script
+# ./build.sh
+
+# Create a new Git repo in dist folder
+cd "$DISTDIR"
+git init
+
+# Set user details
+git config user.name "Ibuild CI"
+git config user.email "ibuild@ibuild.io"
+
+# First commit, .. horray!
+git add .
+git commit -m "Deploy to gh-pages"
+
+# Force push ...
+# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
