9007e79c58e3d04087f970454f58abecc17ea452
Adnan <adnan@websemantics.ca>
2016-03-29 23:14:54 -0400

    Uncomment the build script

--- build.sh
+++ build.sh
@@ -21,32 +21,32 @@ PROFILE="$BASEDIR/src/profiles/app.profile.js"
 # Node.js modules folder
 NODE="$BASEDIR/node_modules"
 
-# echo "Install Bower dependencies"
-#
-# bower install
-#
-# echo "Create theme config file @ src/semantic/src/theme.config "
-#
-# echo '/* Use a common Config file with Semantic UI and Dijit Themes. */
-# @targetFolder : @semanticUiFolder;
-# @import "../../semantic-dojo-styles/less/theme.config";
-# /* End Config */' > src/semantic/src/theme.config
-#
-# echo "Install Node.js packages"
-#
-# if [ -d "$NODE" ]; then
-#     printf '%s\n' "Removing Node.js pacjages folder ($NODE)"
-#     rm -rf "$NODE"
-# fi
-#
-# sudo npm install
-#
-# if [ -d "$DISTDIR" ]; then
-#     printf '%s\n' "Removing old distribution folder ($DISTDIR)"
-#     rm -rf "$DISTDIR"
-# fi
-#
-# mkdir "$DISTDIR"
+echo "Install Bower dependencies"
+
+bower install
+
+echo "Create theme config file @ src/semantic/src/theme.config "
+
+echo '/* Use a common Config file with Semantic UI and Dijit Themes. */
+@targetFolder : @semanticUiFolder;
+@import "../../semantic-dojo-styles/less/theme.config";
+/* End Config */' > src/semantic/src/theme.config
+
+echo "Install Node.js packages"
+
+if [ -d "$NODE" ]; then
+    printf '%s\n' "Removing Node.js pacjages folder ($NODE)"
+    rm -rf "$NODE"
+fi
+
+sudo npm install
+
+if [ -d "$DISTDIR" ]; then
+    printf '%s\n' "Removing old distribution folder ($DISTDIR)"
+    rm -rf "$DISTDIR"
+fi
+
+mkdir "$DISTDIR"
 
 echo "Copy theme assets"
 
@@ -62,33 +62,33 @@ cp -r "$SRCDIR/app/resources" "$DISTDIR"
 echo "Run gulp"
 
 gulp
-#
-# echo "Configuration over. Main application start up"
-#
-# if [ ! -d "$TOOLSDIR" ]; then
-# 	echo "Can't find Dojo build tools -- did you initialise submodules? (git submodule update --init --recursive)"
-# 	exit 1
-# fi
-#
-# echo "Building application with $PROFILE to $DISTDIR."
-#
-# "$TOOLSDIR/build.sh" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
-#
-# cd "$DISTDIR/app"
-#
-# echo "Cleaning unused code"
-#
-# # find . -type f -name '*.uncompressed.js' -print0 | xargs -0 rm -rdf
-# # find . -type f -name '*.consoleStripped.js' -print0 | xargs -0 rm -rdf
-# find . -type f -name 'build-report.txt' -print0 | xargs -0 rm -rdf
-#
-# # Remove app, dijit, dojox and dojo except some files/folders
-# rm -R app
-# rm -R dijit
-# rm -R dojox
-#
-# cd "$DISTDIR/app/dojo"
-# shopt -s extglob
-# rm -R !(dojo.js|nls|resources)
-#
-# echo "Build complete"
+
+echo "Configuration over. Main application start up"
+
+if [ ! -d "$TOOLSDIR" ]; then
+	echo "Can't find Dojo build tools -- did you initialise submodules? (git submodule update --init --recursive)"
+	exit 1
+fi
+
+echo "Building application with $PROFILE to $DISTDIR."
+
+"$TOOLSDIR/build.sh" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
+
+cd "$DISTDIR/app"
+
+echo "Cleaning unused code"
+
+# find . -type f -name '*.uncompressed.js' -print0 | xargs -0 rm -rdf
+# find . -type f -name '*.consoleStripped.js' -print0 | xargs -0 rm -rdf
+find . -type f -name 'build-report.txt' -print0 | xargs -0 rm -rdf
+
+# Remove app, dijit, dojox and dojo except some files/folders
+rm -R app
+rm -R dijit
+rm -R dojox
+
+cd "$DISTDIR/app/dojo"
+shopt -s extglob
+rm -R !(dojo.js|nls|resources)
+
+echo "Build complete"
